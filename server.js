08fbe1e1dbd77f785d352dccdfee272ac02d4786
Muhammad Iqbal <miqbaal1605@gmail.com>
2022-07-15 22:50:43 +0700

    add next middleware

--- server.js
+++ server.js
@@ -38,6 +38,14 @@ app.use(cors());
 app.use(mongoSanitize());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, UPDATE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
